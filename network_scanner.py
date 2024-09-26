360affdc503def5cf6434a3b03dfddbceb9c2b24
bryan_kali <vryan8822@gmail.com>
2024-09-26 06:18:12 -0400

    added scap arp and broadcast

--- network_scanner.py
+++ network_scanner.py
@@ -3,7 +3,21 @@
 import scapy.all as scapy
 
 def scan(ip):
-    arp_request =scapy.ARP()
-    print(arp_request.summary())
+    arp_request = scapy.ARP(pdst=ip)
+    # print(arp_request.summary()) 
+    # scapy.ls(scapy.ARP())
+    # arp_request.show()
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    # scapy.ls(scapy.Ether())
+    # print(broadcast.summary())
+    # broadcast.show()
+    arp_request_broadcast = broadcast/arp_request
+    # print(arp_request_broadcast.summary())
+    # arp_request_broadcast.show()
+    answered_list, unaswered_list = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered_list.summary())
 
-scan("192.168.1.1/24")
+# scan("192.168.1.1/24")
+numbers = [4,3,2,6,775,433,23]
+
+

67e35327693e57aafdc99537d25e0cb9e5ffed9a
bryan_dell_kali <vryan8822@gmail.com/bin/python>
2024-10-03 06:43:53 -0400

    finished projected except for checking parser/optparse

--- network_scanner.py
+++ network_scanner.py
@@ -1,27 +1,35 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+import optparse
+
+# def get_ip():
+#     parser = optparse.OptionParser()
+#     parser.add_option("-i", "--ip_add", dest="ip_address", help="ip address in range to scan for MACs")
+#     (option,arguments) = parser.parse_args()
+#     if not options.ip_address:
+#         parser.error("[-] Please specify an ip address in the range to scan for a MACs")
+#     return options
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    # print(arp_request.summary()) 
-    # scapy.ls(scapy.ARP())
-    # arp_request.show()
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    # scapy.ls(scapy.Ether())
-    # print(broadcast.summary())
-    # broadcast.show()
     arp_request_broadcast = broadcast/arp_request
-    # print(arp_request_broadcast.summary())
-    # arp_request_broadcast.show()
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    # print(answered_list.summary())
 
-    print("IP\t\t\tMAC Address\n=========================================")
+    client_list = []
     for element in answered_list:
-        print(element[1].psrc + "\t\t" + element[1].hwsrc)
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        client_list.append(client_dict)       
+    return client_list
       
+def print_result(results_list):
+        print("IP\t\t\tMAC Address\n=========================================")
+        for client in results_list:
+            print(client["ip"] +"\t\t"+ client["mac"])
 
+# options.get_ip()
+# scan_result = scan(options.ip_address)
 
-
-scan("10.0.2.1/24")
+scan_result = scan("10.0.2.1/24")
+print_result(scan_result)
